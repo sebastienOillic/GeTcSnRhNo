b3dc42dae8c3df2d4386a743e42715164907f8bb
Richard Coudrais <richard.coudrais@gmail.com>
2017-05-02 14:07:33 +0200

    Mise à jour Style Danse

--- src/AppBundle/Entity/StyleDanse.php
+++ src/AppBundle/Entity/StyleDanse.php
@@ -19,6 +19,8 @@ class StyleDanse
      * @var string
      */
     private $nom;
+    
+    private $description;
 
     private $typeDanses;
 
@@ -79,6 +81,24 @@ class StyleDanse
     public function getNom()
     {
         return $this->nom;
+    }
+
+
+    public function setDescription($nom)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
     }
 
   public function addTypeDanse(TypeDanse $typeDanse){
