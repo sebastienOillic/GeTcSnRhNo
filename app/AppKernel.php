dfecae3982cdfe54dcf8a6fc04cc799af5334e18
hertznicolas <hertznico@gmail.com>
2017-05-09 13:19:05 +0200

    merge with crudcours

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,12 +24,10 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-<<<<<<< HEAD
-=======
-            // Alice
-            //$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-            //$bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
->>>>>>> crudcours
+
+
+
+
         }
 
         return $bundles;
